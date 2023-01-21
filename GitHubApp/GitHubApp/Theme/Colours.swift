badf73be1cfc2fec26a2a40360d4650bfa7f3b5e
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-22 00:52:52 +0300

    App Theme is updated.

--- GitHubApp/GitHubApp/Theme/Colours.swift
+++ GitHubApp/GitHubApp/Theme/Colours.swift
@@ -9,10 +9,10 @@ import UIKit
 
 extension UIColor {
     
-    static let viewBGColour     = UIColor(red: 150/255, green: 150/255, blue: 200/255, alpha: 1)
     static let navBarBGColour   = UIColor(red: 10/255, green: 30/255, blue: 30/255, alpha: 1)
     static let navBarTextColour = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
+    static let viewBGColour     = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
+    static let cardBGColour     = UIColor(red: 150/255, green: 150/255, blue: 200/255, alpha: 1)
     static let titleColour      = UIColor(red: 50/255, green: 20/255, blue: 20/255, alpha: 1)
     static let textColour       = UIColor(red: 50/255, green: 50/255, blue: 100/255, alpha: 1)
-    static let viewBGColour     = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
 }
--- GitHubApp/GitHubApp/Theme/Fonts.swift
+++ GitHubApp/GitHubApp/Theme/Fonts.swift
@@ -5,9 +5,11 @@
 //  Created by Oguzhan Yalcin on 21.01.2023.
 //
 
+import UIKit
+
 extension UIFont {
 
-    static let navBarTitleFont  = UIFont.boldSystemFont(ofSize: CGFloat(30).ws)
+    static let navBarTextFont  = UIFont.boldSystemFont(ofSize: CGFloat(30).ws)
     static let titleFont = UIFont.boldSystemFont(ofSize: CGFloat(24).ws)
     static let textFont  = UIFont.systemFont(ofSize: CGFloat(18).ws)
 }
