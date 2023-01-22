59b842f6a7ff5bb1a22d2f7feb4c001efba33c6b
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-22 14:41:27 +0300

    Filter Module is created.

---
+++ GitHubApp/GitHubApp/Modules/Filter/FilterContractor.swift
@@ -0,0 +1,8 @@
+//
+//  FilterContractor.swift
+//  GitHubApp
+//
+//  Created by Oguzhan Yalcin on 22.01.2023.
+//
+
+import Foundation
---
+++ GitHubApp/GitHubApp/Modules/Filter/FilterPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  FilterPresenter.swift
+//  GitHubApp
+//
+//  Created by Oguzhan Yalcin on 22.01.2023.
+//
+
+import Foundation
