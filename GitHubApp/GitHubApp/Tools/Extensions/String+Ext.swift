225cea83099c18bda306999123a027b49f401ed8
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-28 20:08:46 +0300

    string extension funcs are added.

--- GitHubApp/GitHubApp/Tools/Extensions/String+Ext.swift
+++ GitHubApp/GitHubApp/Tools/Extensions/String+Ext.swift
@@ -13,8 +13,22 @@ extension String {
         return CharacterSet(charactersIn: self).isSubset(of: CharacterSet.decimalDigits)
     }
     
+    var marginForButtonTitle: String {
+        return "   \(self)   "
+    }
+    
     func toDouble() -> Double? {
         guard let value = Double(self) else { return nil }
         return value
+    }
+    
+    func toDate(dateFormat: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormat
+        if let date = formatter.date(from: self) {
+            return date
+        } else {
+            return Date()
+        }
     }
 }
